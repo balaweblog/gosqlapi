3a993acf6911db60c5c2f7c853186f99feaa3bad
Balamurugan Alagumalai <balamurugan.alagumalai@gmail.com>
2017-03-28 00:40:31 +0530

    first commit

---
+++ handlers/handlers.go
@@ -0,0 +1,321 @@
+deleted file mode 100644
+@@ -1,318 +0,0 @@
+-package handlers
+-
+-import (
+-	"database/sql"
+-	"encoding/json"
+-	"errors"
+-	"gosqlapi/model"
+-	"io/ioutil"
+-	"net/http"
+-)
+-
+-var (
+-	ErrInvalidInputRequest   = errors.New("http: error reading input request")
+-	ErrInvalidOutputResponse = errors.New("http: error formatting response")
+-	ErrParsingInputRequest   = errors.New("http: error parsing input request")
+-)
+-
+-/* create table in sql */
+-func Createtable(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "CREATE")
+-		val, err := model.ExecuteNonQuery(db, query)
+-
+-		if val != nil && err == nil {
+-			output, _ := json.Marshal("table created successfully")
+-			w.WriteHeader(http.StatusOK)
+-			w.Write(output)
+-		} else {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-	})
+-}
+-
+-/* select from table in sql */
+-func Selecttable(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "SELECT")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+-
+-/* alter table and columns in  sql */
+-func Altertable(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "ALTER")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-
+-		if val != nil && err == nil {
+-			output, _ := json.Marshal("table altered successfully")
+-			w.WriteHeader(http.StatusOK)
+-			w.Write(output)
+-		} else {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-	})
+-}
+-
+-/* insert into table in sql */
+-func Inserttable(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "INSERT")
+-		val, err := model.ExecuteNonQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-
+-		if val != nil && err == nil {
+-			output, _ := json.Marshal("records inserted successfully")
+-			w.WriteHeader(http.StatusOK)
+-			w.Write(output)
+-		} else {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-	})
+-}
+-
+-/* Show all databases in the sql */
+-func Showalldatabases(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		_, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-
+-		query := model.ParseQuery(dat, "SHOWDB")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+-
+-/* what database in use */
+-func CurrentInUseDB(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		_, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-
+-		query := model.ParseQuery(dat, "CURRENTDB")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+-
+-/* show all tables in the database */
+-func ShowalltablesinDb(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		_, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-
+-		query := model.ParseQuery(dat, "SHOWALLTABLES")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+-
+-/* describle individual table in the database */
+-func Describetableindb(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "DESCTABLE")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+-
+-/* explain table in sql */
+-func ExplaintableinDb(db *sql.DB) http.Handler {
+-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+-
+-		req, err := ioutil.ReadAll(r.Body)
+-		defer r.Body.Close()
+-
+-		if err != nil || req == nil {
+-			http.Error(w, ErrInvalidInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		var dat map[string]interface{}
+-		err = json.Unmarshal(req, &dat)
+-		if err != nil {
+-			http.Error(w, ErrParsingInputRequest.Error(), http.StatusBadRequest)
+-			return
+-		}
+-
+-		query := model.ParseQuery(dat, "EXPLAINTABLE")
+-		val, err := model.ExecuteQuery(db, query)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		output, err := json.Marshal(val)
+-		if err != nil {
+-			http.Error(w, err.Error(), http.StatusInternalServerError)
+-			return
+-		}
+-		w.WriteHeader(http.StatusOK)
+-		w.Write(output)
+-	})
+-}
+
---
+++ main.go
@@ -0,0 +1,111 @@
+deleted file mode 100644
+@@ -1,108 +0,0 @@
+-package main
+-
+-import (
+-	"gosqlapi/handlers"
+-	"gosqlapi/model"
+-	"log"
+-	"net/http"
+-)
+-
+-func main() {
+-	db, err := model.NewConnection()
+-
+-	if err != nil {
+-		panic(err)
+-	}
+-	defer db.Close()
+-
+-	mux := http.NewServeMux()
+-	mux.Handle("/showalldatabases", handlers.Showalldatabases(db))
+-	mux.Handle("/createtable", handlers.Createtable(db))
+-	mux.Handle("/selecttable", handlers.Selecttable(db))
+-	mux.Handle("/inserttable", handlers.Inserttable(db))
+-	mux.Handle("/currentdb", handlers.CurrentInUseDB(db))
+-	mux.Handle("/showalltables", handlers.ShowalltablesinDb(db))
+-	mux.Handle("/describetable", handlers.Describetableindb(db))
+-	mux.Handle("/explaintable", handlers.ExplaintableinDb(db))
+-	mux.Handle("/altertable", handlers.Altertable(db))
+-
+-	log.Printf("serving on port 8080")
+-
+-	if err := http.ListenAndServe(":8080", mux); err != nil {
+-		log.Fatal(err)
+-	}
+-}
+-
+-/*
+-create table request
+-{
+-	"table": "employee2422",
+-	"column": [{
+-		"first": "varchar(30)"
+-	}, {
+-		"second": "int"
+-	}]
+-}
+-
+-select table request
+-
+-{
+-	"table": "employee2422",
+-	"column": ["first","second"]
+-}
+-
+-
+-/*
+-{"table": "welcome",
+-	"column": [{
+-		"0": "name1",
+-		"1":"age1"
+-	}],
+-
+-"properties":[{
+-	"name1":"varchar(20)",
+-	"age1":"int not null"
+-}]
+-}
+-*/
+-
+-/*
+-{"table": "welcome",
+-	"column": [{
+-		"0": "name",
+-		"1":"age"
+-	}],
+-	"where":[
+-		{
+-			"StartTag":"(",
+-			"LHS":"name",
+-			"Operator":"=",
+-			"RHS":"'balamurugan'",
+-			"NextOp":"AND",
+-			"EndTag":""
+-		},
+-		{
+-			"StartTag":"",
+-			"LHS":"age",
+-			"Operator":">",
+-			"RHS":"2",
+-			"NextOp":"",
+-			"EndTag":")"
+-		}
+-		]
+-}
+-*/
+-
+-/*
+-{"table": "welcome",
+-	"column": [{
+-		"0": "name1",
+-		"1":"age1"
+-	}],
+-
+-"properties":[{
+-	"name1":"varchar(20)",
+-	"age1":"int not null"
+-}]
+-}
+-*/
+
